e3357b6a096f7b49e03b3e1a6ccb545b6d7cc336
Diego Medina <medina@Diegos-MacBook-Pro.local>
2018-01-31 22:46:25 -0800

    added poster image, and pull down to refresh

--- flixPartOne/flixPartOne/NowPlayingViewController.swift
+++ flixPartOne/flixPartOne/NowPlayingViewController.swift
@@ -7,47 +7,27 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        // table view
+        tableView.dataSource = self
         
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        
-        let task = session.dataTask(with: request) { (data, response, error) in
-            
-            // This will run when the network request returns
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-            
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                
-                // TODO: Get the array of movies
-                let movies = dataDictionary["results"] as! [[String: Any]]
-                
-                // TODO: Store the movies in a property to use elsewhere
-                for movie in movies{
-                    let title = movie["title"] as! String
-                    print(title)
-                }
-                
-                // TODO: Reload your table view data
-                
-                
-            }//data
-            
-        }//task
-        
-        task.resume()
-        
+        fetchMovies()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -65,5 +45,88 @@ class NowPlayingViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        // movie info
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        // poster
+        let posterPathString = movie["poster_path"] as! String
+        let baseUrlString = "https://image.tmdb.org/t/p/w500"
+        let posterURL = URL(string: baseUrlString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterURL)
+        
+        return cell
+    }
 
-}
+    func fetchMovies(){
+        
+        // api call
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+                
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+            }//data
+            
+        }//task
+        
+        task.resume()
+
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl:UIRefreshControl){
+        fetchMovies()
+    }
+    
+    
+} // end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
