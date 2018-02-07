3f135d8c796ed7c24e2529ddc62c78ab1692f48a
Diego Medina <medina@Diegos-MacBook-Pro.local>
2018-02-07 15:17:51 -0800

    implemented another view controller for the grid view, the new view displays the movies in grid view, but now needs to be fixed astetically

---
+++ flixPartOne/flixPartOne/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flixPartOne
+//
+//  Created by Diego Medina on 2/7/18.
+//  Copyright © 2018 Diego Medina. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
